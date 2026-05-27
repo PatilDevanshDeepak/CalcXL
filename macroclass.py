f814e13d7cf2a0d56251a2ba77e3e39b8cc6f76e
Programmer's Attic <119884280+PatilDevanshDeepak@users.noreply.github.com>
2026-05-27 13:21:48 +0530

    Refactor Workbook class and enhance functionality
    Refactor Workbook class with improved comments and functionality. Added methods for sheet management and cell manipulation.

--- macroclass.py
+++ macroclass.py
@@ -1,42 +1,35 @@
-class WorkBook:
-	def __init__(self):				# Constructors of Workbook class
-		self.doc = XSCRIPTCONTEXT.getDocument()
-		self.active = self.doc.CurrentController
-		self.Sheets = self.doc.Sheets
-		self.ActiveSheet = self.active.getActiveSheet()
-		self.SheetsCount = self.Sheets.getCount()
-		self.ActiveSheetName = self.ActiveSheet.getName()
-		self.ActiveSheetIndex = self.Sheets.getElementNames().index(self.ActiveSheetName)+1
+class Workbook:
+	# ----- Contructors for workbook class -----
+	def __init__(self):
+		self.doc = XSCRIPTCONTEXT.getDocument()		# gets the current working document
+		self.active = self.doc.CurrentController 	# gets the current controller which is responsible for getting active cells or sheets
+		self.Sheets = self.doc.Sheets 	# gets all the sheets in the current working doucments as objects
+		self.ActiveSheet = self.active.getActiveSheet() 	# gets active sheet in the current working document
+		self.SheetsCount = self.Sheets.getCount() 	# gets the total number of sheets of the current working document
+		self.ActiveSheetName = self.ActiveSheet.getName() 	# gets the current working sheet's name 
+		self.ActiveSheetIndex = self.Sheets.getElementNames().index(self.ActiveSheetName)+1 	# gets the current working sheet's postions from the sheets 
+		self.MaxRows = self.ActiveSheet.Rows.Count 		# gets the total number of rows in the current working sheets
+		self.MaxColumns = self.ActiveSheet.Columns.Count	 # gets the totol number of columns in the current working sheets
 
 
-	# Funtions for workbook class development
-	def isNull(self, oCell):
-		return True if len(oCell.getString())==0 else False
+	# ----- Macro Functions -----
 
-	@property
-	def ActiveCell(self):
-		return self.active.getSelection()
-
+	# FIXED: Selects Cell, Range or Even Sheets.
+	def Select(self, obj):
+		try:
+			self._select_core(obj)
+		except Exception:
+			try:
+				self._select_core(obj)
+			except:
+				pass		
 	
-	# Macro Functionalities
-	def Select(self, obj):					# IMROVED Now you can Select Cell/Range or a Sheet, Eg: Calc.Select(Calc.Cell(1 , 1)) or Calc.Select(Calc.Range("A1:C10")) or Calc.Select(Calc.SheetName())       
-		if hasattr(obj, "_r"):
-			obj = obj._r
-		if hasattr(obj, "getCellByPosition") and not hasattr(obj, "RangeAddress"):
-			self.ActiveSheet = obj
-			self.active.setActiveSheet(obj)
-			self.ActiveCell = self.Cell(self.Row(obj), self.Column(obj))
-		elif hasattr(obj, "RangeAddress"):
-			self.active.select(obj)
-			self.ActiveCell = self.ActiveSheet.getCellByPosition(
-				obj.RangeAddress.StartColumn,
-				obj.RangeAddress.StartRow
-			)			
-	
-	def Cell(self, row, col):					# Return a Cell with given row and column as an Object Eg Calc.Cell(1 , 1) will return cell A1
+	# Return a Cell with given row and column as an Object Eg Calc.Cell(1 , 1) will return cell A1
+	def Cell(self, row, col):
 		return self.ActiveSheet.getCellByPosition(col-1, row-1)
 		
-	def Range(self, fromCell=None , toCell=None):					# IMPROVED Now it can set value to a Range Eg: Calc.Range("A1:A10").String = "Hello word"  
+	# IMPROVED Now it can set value to a Range Eg: Calc.Range("A1").String = "Hello word" or Calc.Range("A1:"B10") = "Hello world" or Calc.Range(Cell(1 , 1) , Cell(20 , 1))
+	def Range(self, fromCell=None , toCell=None): 	
 		if toCell==None:
 			rng = self.ActiveSheet.getCellRangeByName(fromCell)
 		else:
@@ -56,9 +49,10 @@ class WorkBook:
 					elif n == "Formula": self._r.setFormulaArray(data)
 				else:
 					setattr(self._r, n, v)
-		return _R(rng)
+		return _R(rng)	
 	
-	def Offset(self, rowIndex, colIndex):					# This acts as ActiveCell.Offset in VBA 
+	# This acts like ActiveCell.Offset in VBA, eg: Calc.Offset(0 , 1) will return the cell from the next column as an object
+	def Offset(self, rowIndex, colIndex): 
 		maxColumns = self.ActiveSheet.Columns.Count - 1
 		maxRows = self.ActiveSheet.Rows.Count - 1
 		newRow = self.ActiveCell.RangeAddress.StartRow + rowIndex
@@ -67,30 +61,38 @@ class WorkBook:
 		newCol = max(0, min(newCol, maxColumns))
 		return self.ActiveSheet.getCellByPosition(newCol, newRow)
 			
-	def Row(self, oCell):					# Returns the Start Row of a Cell/Range 
+	# Returns the Row Index for the given Cell/Range
+	def Row(self, oCell): 
 		return 	oCell.RangeAddress.StartRow + 1
 
-	def Column(self, oCell):		# Returns the Start Column of a Cell/Range 
+	# Returns the Column Index for a given Cell/Range
+	def Column(self, oCell): 
 		return oCell.RangeAddress.StartColumn + 1
 	
-	def EndRow(self , oCell):					# NEW Returns End Row of a Range as an int 
-		return self.RangeAddress.EndRow + 1
+	# Returns the End Row Index for a given Cell/Range
+	def EndRow(self , oCell): 
+		return oCell.RangeAddress.EndRow + 1
 	
-	def EndColumn(self , oCell):					# NEW Returns End Column of a Range as an int 
-		return self.RangeAddress.EndColumn + 1
+	# Returns the End Column Index for a given Cell/Range
+	def EndColumn(self , oCell):
+		return oCell.RangeAddress.EndColumn + 1
 	
-	def RangeAddress(self , absolute=False):			# NEW Returns the Address of a Cell/Range 
+	# Returns the Address given Cell/Range
+	def RangeAddress(self , absolute=False):
 		if absolute:
 			return self.getActiveSelection().AbsoluteName 
 		return (self.active.getSelection().AbsoluteName).replace("$" , "")
 
-	def SheetName(self , sheetName):					# NEW Returns the Sheet as an Object searched by Sheet Name
+	# Returns the Sheet as an Object searched by Sheet Name
+	def SheetName(self , sheetName):
 		return self.Sheets.getByName(sheetName)
 	
-	def SheetIndex(self , index):					# NEW Returns the Sheet as an Object searched by Sheet Index
+	# Returns the Sheet as an Object searched by Sheet Index
+	def SheetIndex(self , index):
 		return self.Sheets.getByIndex(index-1)
 
-	def CreateSheet(self, sheetName, setBefore=None):					# NEW Creates a new sheet by giving sheet name, and using set before attribute Eg Calc.CreateSheet("SHEETNAME" , 1) then the sheet will be created on index 2
+	# NEW Creates a new sheet by giving sheet name, and using set before attribute Eg Calc.CreateSheet("SHEETNAME" , 1) then the sheet will be created on index 2
+	def CreateSheet(self, sheetName, setBefore=None):
 		if setBefore is None:
 			setBefore = self.ActiveSheetIndex - 1
 		elif setBefore > self.SheetsCount:
@@ -104,10 +106,12 @@ class WorkBook:
 		self.SheetsCount = self.Sheets.getCount()
 		return newSheet
 	
+	# Renames a Sheet by replacing its old name with a new one
 	def RenameSheet(self , oldName , newName):
 		Sheet = self.SheetName(oldName)
 		Sheet.setName(newName)
 	
+	# Deletes a sheet by addr, an addr could be sheet's name or index
 	def DeleteSheet(self , addr):
 		if isinstance(addr , int):
 			addr -= 1
@@ -116,62 +120,9 @@ class WorkBook:
 		elif isinstance(addr , str):
 			self.SearchSheetByName(addr).removeByName(addr)
 
-	def ClearContents(self, obj, clearAll=False):		# Note: This Function Have Bugs !!!
-		# Clear All Values can be:
-		FLAG_STRING = 1
-		FLAG_VALUE = 2
-		FLAG_FORMULA = 4
-		FLAG_HARDATTR = 16  # formatting
-		FLAG_ALL = 7	# clear all
-
-		## NOTE: if the clearAll is not given it will consider 7 as value
-
-		if clearAll:
-			flags = FLAG_STRING | FLAG_VALUE | FLAG_FORMULA | FLAG_HARDATTR
-		else:
-			flags = FLAG_STRING | FLAG_VALUE | FLAG_FORMULA   # = 7
-	
-	def clcToRight(self , oCell=None):	# This function is like xlToRight in VBA   Note: This Function Have Bugs !!!
-		nullCell = self.isNull(self.ActiveCell)
-		count = 0
-		while count <= self.getEndColumn()-1:
-			nullCell = self.isNull(self.Offset(0 , count))
-			if nullCell != self.isNull(self.ActiveCell):
-				break
-			count += 1
-		return self.Offset(0 , count-1 if not self.isNull(self.ActiveCell) else count)
-	
-	def clcToLeft(self):	# This function is like xlToLeft in VBA    Note: This Function Have Bugs !!!
-		nullCell = self.isNull(self.ActiveCell)
-		count = 0
-		while abs(count) <= self.getStartColumn()-1:
-			nullCell = self.isNull(self.Offset(0 , count))
-			if nullCell != self.isNull(self.ActiveCell):
-				break
-			count -= 1
-		return self.Offset(0 , count+1 if not self.isNull(self.ActiveCell) else count)
-	
-	def clcDown(self):	# This function is like xlDown in VBA		Note: This Function Have Bugs !!!
-		nullCell = self.isNull(self.ActiveCell)
-		count = 0
-		while count <= self.getEndRow()-1:
-			nullCell = self.isNull(self.Offset(count , 0))
-			if nullCell != self.isNull(self.ActiveCell):
-				break
-			count += 1
-		return self.Offset(count-1 if not self.isNull(self.ActiveCell) else count , 0)
-	
-	def clcUp(self):	# This function is like xlUp in VBA		Note: This Function Have Bugs In It !!!
-		nullCell = self.isNull(self.ActiveCell)
-		count = 0
-		while count <= self.getStartRow()-1:
-			nullCell = self.isNull(self.Offset(count , 0))
-			if nullCell != self.isNull(self.ActiveCell):
-				break
-			count -= 1
-		return self.Offset(count+1 if not self.isNull(self.ActiveCell) else count , 0)
-
-	def WorksheetFunctions(self, functionName , args):       # Calc.WorksheetFunctions("functionName in capital" , [args as tuple]) for eg: Calc.Offset(0 , -1).Value = Calc.WorksheetFunctions("ISBLANK" , [Calc.ActiveCell.getString()]. IF you are giving cell ref. always add .getString() ahead
+	# Calc.WorksheetFunctions("functionName in capital" , [args as tuple]) for eg: Calc.Offset(0 , -1).Value = Calc.WorksheetFunctions("ISBLANK" , [Calc.ActiveCell.getString()].
+	# IF you are giving cell ref. always add .getString() ahead to get the cell contents as String
+	def WorksheetFunctions(self, functionName , args):
 		smgr = XSCRIPTCONTEXT.getComponentContext().ServiceManager
 		func_access = smgr.createInstanceWithContext(
 			"com.sun.star.sheet.FunctionAccess",
@@ -180,7 +131,42 @@ class WorkBook:
 		return func_access.callFunction(functionName, tuple(args))
 
 
-Calc = WorkBook()
+	# ----- Funtions for workbook class extension -----
 
+	# Rentrus Boolean value to check the 
+	def isNull(self, oCell):
+		return True if len(oCell.getString())==0 else False
+
+	# Fixed: Returns ActiveCell as and Object
+	@property
+	def ActiveCell(self):
+		return self.active.getSelection()
+
+	# Fixed: Raw Select Function that selects Cell/Range. NOTE: using this function might get error while using  
+	def _select_core(self, obj):
+		if hasattr(obj, "_r"):
+			obj = obj._r
+		if hasattr(obj, "getCellByPosition") and not hasattr(obj, "RangeAddress"):
+			self.ActiveSheet = obj
+			self.active.setActiveSheet(obj)
+			self.ActiveCell = self.Cell(self.Row(obj), self.Column(obj))
+		elif hasattr(obj, "RangeAddress"):
+			self.active.select(obj)
+			self.ActiveCell = self.ActiveSheet.getCellByPosition(
+				obj.RangeAddress.StartColumn,
+				obj.RangeAddress.StartRow
+			)
+
+# Add the below code at the bottom
+Calc = Workbook()
+
+
+# Your Macro will start from any function you create outside the Class
 def Automate():
-	Calc.Range(fromCell=Calc.Cell(1 , 1) , toCell=Calc.Cell(10 , 2)).String = "Hello world"
+	# Your Macro start from here
+	Calc.ActiveCell.String = Calc.maxColumns
+
+
+# If you want to add another macro, you can create a new function
+def test():
+	Calc.Select(Calc.Range("A1"))
